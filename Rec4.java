6522c142a1de0fd71364d866dc0187b011b28366
Rubham Soni <104519090+RubhamZ11@users.noreply.github.com>
2023-05-21 16:36:57 +0530

    Add files via upload

---
+++ Rec4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class P4
+{
+    public static void f(int arr[],int i)
+    {
+        if(i==-1)
+        return;
+        else
+        {
+            System.out.print(arr[i]+" ");
+            f(arr,i-1);
+        }
+    }
+    public static void main(String[] args) 
+    {
+        int arr[]={8,3,0,3,1};
+        f(arr,arr.length-1);
+    }
